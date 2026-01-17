ceeec81182dbce60ead7aa6ed94d74f9b4ca984e
xu233333 <18510478058@163.com>
2026-01-17 23:08:37 +0800

    添加新形态(FamiliarFoxPurify) 添加模型 明天写一些Power

--- src/main/java/xu_mod/init/InitForm.java
+++ src/main/java/xu_mod/init/InitForm.java
@@ -1,11 +1,15 @@
 package xu_mod.init;
 
 import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormBase;
+import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormGroup;
+import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormPhase;
+import net.onixary.shapeShifterCurseFabric.player_form.RegPlayerForms;
 import xu_mod.data.form.FamiliarFoxPurify;
 import xu_mod.SSCXuAddon;
 
 public class InitForm {
-    public static PlayerFormBase FamiliarFoxPurify = new FamiliarFoxPurify(SSCXuAddon.identifier("familiar_fox_purify"));
+    public static PlayerFormBase FamiliarFoxPurify = RegPlayerForms.registerPlayerForm(new FamiliarFoxPurify(SSCXuAddon.identifier("familiar_fox_purify")).setPhase(PlayerFormPhase.PHASE_3));
+    public static PlayerFormGroup FamiliarFoxPurifyGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("familiar_fox_purify_form")).addForm(FamiliarFoxPurify, 3)));
 
     public static void init() {}
 }
