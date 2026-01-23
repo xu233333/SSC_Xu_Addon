d16a14f6746a0e538db5495215f636f963a329d0
xu233333 <18510478058@163.com>
2026-01-23 19:26:27 +0800

    吸血鬼蝙蝠渲染+注册形态

---
+++ src/main/java/xu_mod/SSCXuAddon/data/form/BatVampire.java
@@ -0,0 +1,12 @@
+package xu_mod.SSCXuAddon.data.form;
+
+import net.minecraft.util.Identifier;
+import net.onixary.shapeShifterCurseFabric.player_form.forms.Form_Bat3;
+
+public class BatVampire extends Form_Bat3 {
+    // 蝙蝠(吸血鬼)
+
+    public BatVampire(Identifier formID) {
+        super(formID);
+    }
+}
--- src/main/java/xu_mod/SSCXuAddon/init/Init_Form.java
+++ src/main/java/xu_mod/SSCXuAddon/init/Init_Form.java
@@ -4,12 +4,15 @@ import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormBase;
 import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormGroup;
 import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormPhase;
 import net.onixary.shapeShifterCurseFabric.player_form.RegPlayerForms;
+import xu_mod.SSCXuAddon.data.form.BatVampire;
 import xu_mod.SSCXuAddon.data.form.FamiliarFoxPurify;
 import xu_mod.SSCXuAddon.SSCXuAddon;
 
 public class Init_Form {
     public static PlayerFormBase FamiliarFoxPurify = RegPlayerForms.registerPlayerForm(new FamiliarFoxPurify(SSCXuAddon.identifier("familiar_fox_purify")).setPhase(PlayerFormPhase.PHASE_3));
     public static PlayerFormGroup FamiliarFoxPurifyGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("familiar_fox_purify_form")).addForm(FamiliarFoxPurify, 3)));
+    public static PlayerFormBase BatVampire = RegPlayerForms.registerPlayerForm(new BatVampire(SSCXuAddon.identifier("bat_vampire")).setPhase(PlayerFormPhase.PHASE_3));
+    public static PlayerFormGroup BatVampireGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("bat_vampire_form")).addForm(BatVampire, 3)));
 
     public static void init() {}
 }
