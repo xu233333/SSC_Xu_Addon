784367c66dd63bc1bff5f25e7bff97116fff38b2
xu233333 <18510478058@163.com>
2026-02-02 16:44:06 +0800

    猫形态代码 又发现一个Bug 修完再写拓展

---
+++ src/main/java/xu_mod/SSCXuAddon/data/form/CatVF.java
@@ -0,0 +1,12 @@
+package xu_mod.SSCXuAddon.data.form;
+
+import net.minecraft.util.Identifier;
+import net.onixary.shapeShifterCurseFabric.player_form.forms.Form_FeralCatSP;
+
+public class CatVF extends Form_FeralCatSP {
+    // 猫(村庄之友)
+
+    public CatVF(Identifier formID) {
+        super(formID);
+    }
+}
--- src/main/java/xu_mod/SSCXuAddon/init/Init_Form.java
+++ src/main/java/xu_mod/SSCXuAddon/init/Init_Form.java
@@ -1,10 +1,8 @@
 package xu_mod.SSCXuAddon.init;
 
-import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormBase;
-import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormGroup;
-import net.onixary.shapeShifterCurseFabric.player_form.PlayerFormPhase;
-import net.onixary.shapeShifterCurseFabric.player_form.RegPlayerForms;
+import net.onixary.shapeShifterCurseFabric.player_form.*;
 import xu_mod.SSCXuAddon.data.form.BatVampire;
+import xu_mod.SSCXuAddon.data.form.CatVF;
 import xu_mod.SSCXuAddon.data.form.FamiliarFoxPurify;
 import xu_mod.SSCXuAddon.SSCXuAddon;
 
@@ -13,6 +11,8 @@ public class Init_Form {
     public static PlayerFormGroup FamiliarFoxPurifyGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("familiar_fox_purify_form")).addForm(FamiliarFoxPurify, 3)));
     public static PlayerFormBase BatVampire = RegPlayerForms.registerPlayerForm(new BatVampire(SSCXuAddon.identifier("bat_vampire")).setPhase(PlayerFormPhase.PHASE_3).setHasSlowFall(true).setOverrideHandAnim(true));
     public static PlayerFormGroup BatVampireGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("bat_vampire_form")).addForm(BatVampire, 3)));
+    public static PlayerFormBase FeralCatVF = RegPlayerForms.registerPlayerForm(new CatVF(SSCXuAddon.identifier("feral_cat_vf")).setPhase(PlayerFormPhase.PHASE_SP).setBodyType(PlayerFormBodyType.FERAL));
+    public static PlayerFormGroup FeralCatVFGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("feral_cat_vf_form")).addForm(FeralCatVF, 5)));
 
     public static void init() {}
 }
