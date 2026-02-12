b2a9f6be93e00c64bf8305956055298ef0105fc6
xu233333 <18510478058@163.com>
2026-02-12 22:55:28 +0800

    悦灵 Form 注册 更新TODO 能力等之后再删减

---
+++ src/main/java/xu_mod/SSCXuAddon/data/form/AllayEngineer.java
@@ -0,0 +1,11 @@
+package xu_mod.SSCXuAddon.data.form;
+
+import net.minecraft.util.Identifier;
+import net.onixary.shapeShifterCurseFabric.player_form.forms.Form_Allay;
+
+public class AllayEngineer extends Form_Allay {
+    // 悦灵((空间)工程师)
+    public AllayEngineer(Identifier formID) {
+        super(formID);
+    }
+}
--- src/main/java/xu_mod/SSCXuAddon/init/Init_Form.java
+++ src/main/java/xu_mod/SSCXuAddon/init/Init_Form.java
@@ -1,6 +1,7 @@
 package xu_mod.SSCXuAddon.init;
 
 import net.onixary.shapeShifterCurseFabric.player_form.*;
+import xu_mod.SSCXuAddon.data.form.AllayEngineer;
 import xu_mod.SSCXuAddon.data.form.BatVampire;
 import xu_mod.SSCXuAddon.data.form.CatVF;
 import xu_mod.SSCXuAddon.data.form.FamiliarFoxPurify;
@@ -13,6 +14,8 @@ public class Init_Form {
     public static PlayerFormGroup BatVampireGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("bat_vampire_form")).addForm(BatVampire, 3)));
     public static PlayerFormBase FeralCatVF = RegPlayerForms.registerPlayerForm(new CatVF(SSCXuAddon.identifier("feral_cat_vf")).setPhase(PlayerFormPhase.PHASE_SP).setBodyType(PlayerFormBodyType.FERAL));
     public static PlayerFormGroup FeralCatVFGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("feral_cat_vf_form")).addForm(FeralCatVF, 5)));
+    public static PlayerFormBase AllayEngineer = RegPlayerForms.registerPlayerForm(new AllayEngineer(SSCXuAddon.identifier("allay_engineer")).setPhase(PlayerFormPhase.PHASE_SP));
+    public static PlayerFormGroup AllayEngineerGroup = RegPlayerForms.registerPlayerFormGroup((new PlayerFormGroup(SSCXuAddon.identifier("allay_engineer_form")).addForm(AllayEngineer, 5)));
 
     public static void init() {}
 }
